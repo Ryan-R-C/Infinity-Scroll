5fe58674af56d62748f24cf01ae19715be7aa345
ryang <ryan.r.c.339ac@gmail.com>
2021-11-05 08:47:52 -0300

    (filter) refratored the inputValue and post title and texts / includes is sence sensitive

--- filter.js
+++ filter.js
@@ -1,13 +1,13 @@
 const filter = document.querySelector('#filter')
 filter.addEventListener('input', event => {
-    const inputValue = event.target.value
+    const inputValue = event.target.value.toLowerCase
     console.log(inputValue);
 
     const posts = document.querySelectorAll('.post')
 
     posts.forEach((post, index) => {
-        const postTitle = post.querySelector('.post-title').textContent
-        const postBody = post.querySelector('.post-body').textContent
+        const postTitle = post.querySelector('.post-title').textContent.toLowerCase
+        const postBody = post.querySelector('.post-body').textContent.toLowerCase
 
         if(postTitle.includes(inputValue) || postBody.includes(inputValue)){
             console.log(`post ${index + 1 }, value searched ${inputValue}`);
