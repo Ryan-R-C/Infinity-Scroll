2a22d707c360df18588aa76eabb6976daaa5a999
ryang <ryan.r.c.339ac@gmail.com>
2021-11-05 08:52:53 -0300

    fixed a typo and added a background red to match

--- filter.js
+++ filter.js
@@ -1,17 +1,16 @@
 const filter = document.querySelector('#filter')
 filter.addEventListener('input', event => {
-    const inputValue = event.target.value.toLowerCase
+    const inputValue = event.target.value.toLowerCase()
     console.log(inputValue);
 
     const posts = document.querySelectorAll('.post')
 
-    posts.forEach((post, index) => {
-        const postTitle = post.querySelector('.post-title').textContent.toLowerCase
-        const postBody = post.querySelector('.post-body').textContent.toLowerCase
+    posts.forEach(post=> {
+        const postTitle = post.querySelector('.post-title').textContent.toLowerCase()
+        const postBody = post.querySelector('.post-body').textContent.toLowerCase()
 
         if(postTitle.includes(inputValue) || postBody.includes(inputValue)){
-            console.log(`post ${index + 1 }, value searched ${inputValue}`);
-
+            post.style.backgroundColor = "red"
         }
     })
 })
