0966405ff7da102aa200d6d456e1d77dc0fad632
ryang <ryan.r.c.339ac@gmail.com>
2021-11-08 11:10:36 -0300

    (filter.js) refratored the code -> handle input.forEach calls showMatchedPosts

--- filter.js
+++ filter.js
@@ -1,26 +1,28 @@
 const filter = document.querySelector('#filter')
 
+const showMatchedPosts = post => {
+    const postTitle = post.querySelector('.post-title').textContent.toLowerCase()
+    const postBody = post.querySelector('.post-body').textContent.toLowerCase()
+
+    //Check if the Post Content has the inputValue
+    if(postTitle.includes(inputValue) || postBody.includes(inputValue)){//Incluedes is casa sensitive
+
+        //the elements which includes the inputValue will continue to be displayed in screen:
+        post.style.display = "flex"
+        //it prevents this function to be executed again and again:
+        return
+    }
+    //all elements that does not have the inputValue will not be displayed
+    post.style.display = "none"
+}
+
 const handleInputValue = event => {
     const inputValue = event.target.value.toLowerCase()
     console.log(inputValue);
 
     const posts = document.querySelectorAll('.post')
 
-    posts.forEach(post=> {
-        const postTitle = post.querySelector('.post-title').textContent.toLowerCase()
-        const postBody = post.querySelector('.post-body').textContent.toLowerCase()
-
-        //Check if the Post Content has the inputValue
-        if(postTitle.includes(inputValue) || postBody.includes(inputValue)){//Incluedes is casa sensitive
-
-            //the elements which includes the inputValue will continue to be displayed in screen:
-            post.style.display = "flex"
-            //it prevents this function to be executed again and again:
-            return
-        }
-        //all elements that does not have the inputValue will not be displayed
-        post.style.display = "none"
-    })
+    posts.forEach(showMatchedPosts)
 }
 
-filter.addEventListener('input', handleInputValue())
+filter.addEventListener('input', handleInputValue)
